ad35855a868aad54939498ee682bf2be02e9811e
Daisy Yi <daisyyi@rocketmail.com>
2022-08-16 11:24:26 -0700

    commit tree 1fee21d65770acce73e6dba5819ddc7c0333075c

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Bob",
+    streetAddress: "212 Mark Street", 
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmployee = {...object};
+    newEmployee[key]=value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key]=value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newEmployee2 = {...object};
+    delete newEmployee2[key];
+    return newEmployee2;
+}
+
+function destructivelyDeleteEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
